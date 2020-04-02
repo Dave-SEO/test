2b0d1730e5b6bf3d5d05148992d25b3396a03e34
dave <1393193549@qq.com>
2020-04-02 16:30:03 +0800

    dist-a

--- test1.sh
+++ test1.sh
@@ -1,7 +1,7 @@
 git rm -r --cached test
-git commit -m 'delete test dir-1'
+git commit -m 'delete test dir-2'
 git push -u origin master
 yarn build
 git add .
-git commit -m 'dist'
+git commit -m 'dist-a'
 git push
