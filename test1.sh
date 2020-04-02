afbcec20b2255fe5696cc2dfb5d9320a3ff5db6f
dave <1393193549@qq.com>
2020-04-02 16:36:56 +0800

    build-c

--- test1.sh
+++ test1.sh
@@ -1,7 +1,7 @@
-git rm -r --cached test
-git commit -m 'delete test dir-4'
+git rm -r  build
+git commit -m 'delete build dir-4'
 git push -u origin master
 yarn build
 git add .
-git commit -m 'dist-c'
+git commit -m 'build-c'
 git push
